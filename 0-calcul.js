5cecc5aa877a4c8d094070c59d2a67e09b56fea5
mbaldeh1 <muhammadoubaldeh@gmail.com>
2023-11-22 14:55:17 +0000

    Unittests in JS

--- 0-calcul.js
+++
@@ -1,9 +0,0 @@
-/**
- * Task 0: Basic test with mocha and Node assertion library
- * function calculateNumber that takes a type and two numbers
- * Returns the addition of the two numbers rounded
- */
-
-const calculateNumber = (a, b) => Math.round(a) + Math.round(b);
-
-module.exports = calculateNumber;
